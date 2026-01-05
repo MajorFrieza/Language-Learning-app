6e741842030a6946d3dc077f2f5dbbb58d4815e4
Mac <136963002+xZephyros@users.noreply.github.com>
2026-01-05 11:17:29 +0800

    Added comments

--- src/Gamifiable.java
+++ src/Gamifiable.java
@@ -3,13 +3,14 @@
  * Contract for gamification systems in the application.
  * Defines methods that all gamification implementations must provide.
  * 
- * Creator: Group 3 Benny Java
+ * Creator: Hafizh
  * Purpose: Establish interface for gamification feature implementations.
  */
 
 public interface Gamifiable {
     /**
      * Add points to the score for gamification.
+     * 
      * @param points The number of points to add
      */
     void addScore(int points);
